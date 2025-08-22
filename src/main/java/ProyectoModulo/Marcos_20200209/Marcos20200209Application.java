9292d52edb6d37d3a8a2ab10860010731525ec39
Torres093 <marcosalejandrot583@gmail.com>
2025-08-22 10:20:56 -0600

    primer commit

---
+++ src/main/java/ProyectoModulo/Marcos_20200209/Marcos20200209Application.java
@@ -0,0 +1,13 @@
+package ProyectoModulo.Marcos_20200209;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Marcos20200209Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Marcos20200209Application.class, args);
+	}
+
+}
---
+++ src/test/java/ProyectoModulo/Marcos_20200209/Marcos20200209ApplicationTests.java
@@ -0,0 +1,13 @@
+package ProyectoModulo.Marcos_20200209;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Marcos20200209ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
